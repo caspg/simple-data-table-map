3a6e8015f4f6e2177eb10b35c4791cec8da77188
caspg <kacper.golinski@gmail.com>
2016-01-26 16:21:33 +0100

    initial setup - simple-data-grid-map

---
+++ simple-data-grid-map/app/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './components/Hello.jsx';
+
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('app')
+);
---
+++ simple-data-grid-map/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: './app/main.js',
+  output: { path: '/', filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
