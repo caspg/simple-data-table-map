47a2cb804c9bdb3fad4942da37bc44c6f023a65e
caspg <kacper.golinski@gmail.com>
2016-01-26 18:42:21 +0100

    update DataTableInput

--- simple-data-grid-map/app/components/DataTableInput.jsx
+++ simple-data-grid-map/app/components/DataTableInput.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
 export default class DataTableInput extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { value: this.props.value };
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleOnBlur = this.handleOnBlur.bind(this);
+  }
+  handleOnChange(event){
+    const newValue = event.target.value;
+    if (newValue === this.state.value) return;
+    if (!/^[+-]?\d*(\.\d*)?$/.test(newValue)) return;
+    this.setState({value: newValue});
+  }
+  handleOnBlur(event){
+    const newValue = parseFloat(event.target.value) || 0;
+    this.setState({ value: newValue });
+    console.log('handleOnBlur: setState() should be called only when value is changed!');
+  }
   render() {
     return (
-      <input type="text"/>
+      <input 
+        type="text"
+        value={this.state.value}
+        onChange={this.handleOnChange}
+        onBlur={this.handleOnBlur}
+      />
     );
   }
+}
+
+DataTableInput.propTypes = {
+  value: React.PropTypes.number.isRequired
 }
